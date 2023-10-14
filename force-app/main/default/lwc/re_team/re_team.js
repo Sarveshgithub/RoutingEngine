4a14276ee759284e1a0b619d02d29a64394f5670
Sarvesh <sarveshkumar574@gmail.com>
2023-10-14 20:51:54 +0530

    added data

--- force-app/main/default/lwc/re_team/re_team.js
+++ force-app/main/default/lwc/re_team/re_team.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement ,api} from 'lwc';
+import generateData from './generateData';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 
-export default class Re_team extends LightningElement {}
+const columns = [
+    { label: 'Label', fieldName: 'name' },
+    { label: 'Website', fieldName: 'website', type: 'url' },
+    { label: 'Phone', fieldName: 'phone', type: 'phone' },
+    { label: 'Balance', fieldName: 'amount', type: 'currency' },
+    { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
+];
+
+export default class Re_team extends LightningElement {
+    data = [];
+    columns = columns;
+    @api objectApiName ='Account';
+
+    fields = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
+
+    handleSuccess(event) {
+        const evt = new ShowToastEvent({
+            title: 'Account created',
+            message: 'Record ID: ' + event.detail.id,
+            variant: 'success',
+        });
+        this.dispatchEvent(evt);
+    }
+    connectedCallback() {
+        const data = generateData({ amountOfRecords: 100 });
+        this.data = data;
+    }
+
+    handleNewTeam(){
+
+    }
+}
