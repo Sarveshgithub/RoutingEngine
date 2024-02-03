ec87c37669c9cf04eda801181649badafc093716
sarveshspoton <153097096+sarveshspoton@users.noreply.github.com>
2024-02-03 20:24:04 +0530

    add changes

--- force-app/main/default/lwc/re_team/generateData.js
+++ force-app/main/default/lwc/re_team/generateData.js
@@ -7,7 +7,7 @@ export default function generateData({ amountOfRecords }) {
             phone: `${Math.floor(Math.random() * 9000000000) + 1000000000}`,
             closeAt: new Date(
                 Date.now() + 86400000 * Math.ceil(Math.random() * 20)
-            ),
+            )
         };
     });
 }
