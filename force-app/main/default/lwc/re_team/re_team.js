2c5ee8af8c8c495569dcf8f0b592e427c7ed37bb
sarveshgithub <sarveshkumar574@gmail.com>
2025-01-14 21:07:09 +0530

    add table action edit delete

--- force-app/main/default/lwc/re_team/re_team.js
+++ force-app/main/default/lwc/re_team/re_team.js
@@ -1,88 +1,103 @@
-import { LightningElement, api, wire } from "lwc";
-import { NavigationMixin } from "lightning/navigation";
-import fetchTeams from "@salesforce/apex/RE_EngineController.fetchTeams";
+import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import fetchTeams from '@salesforce/apex/RE_EngineController.fetchTeams';
+import { refreshApex } from '@salesforce/apex';
 
 const columns = [
     {
-        label: "Name", fieldName: "Id", type: 'url', typeAttributes: {
+        label: 'Name',
+        fieldName: 'Id',
+        type: 'url',
+        typeAttributes: {
             label: {
-                fieldName: 'Name',
-            }, target: '_blank', // Opens the URL in a new tab
+                fieldName: 'Name'
+            },
+            target: '_blank', // Opens the URL in a new tab
             onclick: 'handleUrlClick'
         }
     },
-    { label: "Active", fieldName: "Active__c", type: "checkbox" },
-    { label: "Object", fieldName: "Object__c" },
+    { label: 'Active', fieldName: 'Active__c', type: 'checkbox' },
+    { label: 'Object', fieldName: 'Object__c' },
     {
-        type: "button",
-        fixedWidth: 150,
+        type: 'button-icon',
+        fixedWidth: 50,
         typeAttributes: {
-            label: 'Edit',
-            name: 'edit',
-            variant: 'brand'
+            iconName: 'utility:edit',
+            name: 'edit'
         }
     },
+    {
+        type: 'button-icon',
+        fixedWidth: 50,
+        typeAttributes: {
+            iconName: 'utility:delete',
+            name: 'delete',
+            iconClass: 'slds-icon-text-error'
+        }
+    }
 ];
 
 export default class Re_team extends NavigationMixin(LightningElement) {
     data = [];
     columns = columns;
-    show = true
+    show = true;
     team;
 
     @wire(fetchTeams)
     wiredMyData({ error, data }) {
         if (data) {
-            console.log("data:::", data);
+            console.log('data:::', data);
             this.data = data;
         } else if (error) {
-            console.error("Error loading data: " + JSON.stringify(error));
+            console.error('Error loading data: ' + JSON.stringify(error));
         }
     }
     get() {
-        console.log("teams:::", this.teams);
+        console.log('teams:::', this.teams);
     }
     check() {
-        this.show = !this.show
+        this.show = !this.show;
     }
     handleSuccess(event) {
         const evt = new ShowToastEvent({
-            title: "Team Created",
-            message: "Record ID: " + event.detail.id,
-            variant: "success"
+            title: 'Team Created',
+            message: 'Record ID: ' + event.detail.id,
+            variant: 'success'
         });
         this.dispatchEvent(evt);
     }
     connectedCallback() {
         const urlCells = this.template.querySelectorAll('a.slds-truncate');
-        urlCells.forEach(urlCell => {
+        urlCells.forEach((urlCell) => {
             urlCell.addEventListener('click', this.handleUrlClick.bind(this));
         });
     }
     handleUrlClick(event) {
-        console.log('evnet', event)
+        console.log('evnet', event);
     }
     callRowAction(event) {
         const selectedValue = event.detail;
         const actionName = event.detail.action.name;
-        console.log('event>>', event, selectedValue.row, actionName)
+        console.log('event>>', event, selectedValue.row, actionName);
         this.team = selectedValue.row;
-        this.show = false
-        if (actionName === "ActionName") {
+        this.show = false;
+        if (actionName === 'ActionName') {
             // init your modal here
             this.openModal = true;
         }
     }
     handleNewTeam() {
         this[NavigationMixin.Navigate]({
-            type: "standard__objectPage",
+            type: 'standard__objectPage',
             attributes: {
-                objectApiName: "Team__c",
-                actionName: "new"
+                objectApiName: 'Team__c',
+                actionName: 'new'
             },
             state: {
-                navigationLocation: "RELATED_LIST"
+                navigationLocation: 'RELATED_LIST'
             }
+        }).then(() => {
+            refreshApex(this.data);
         });
     }
 }
